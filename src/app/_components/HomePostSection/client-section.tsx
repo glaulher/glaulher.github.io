d34a76a4ae20badeaacb30a33d12619146a5f99b
Glaulher Ramalho Medeiros <glaulher@localhost.localdomain>
2025-02-05 22:48:58 -0300

    feat:update align-items

--- src/app/_components/HomePostSection/client-section.tsx
+++ src/app/_components/HomePostSection/client-section.tsx
@@ -28,65 +28,56 @@ export const ClientHomePostSection = ({ allPosts }: { allPosts: Post[] }) => {
 
   return (
     <>
-      <div className="relative flex items-center mr-2 justify-end ml-auto ">
-        <input
-          type="text"
-          placeholder="Pesquisar"
-          value={searchValue}
-          onChange={(e) => setSearchValue(e.target.value)}
-          className="pl-2  pr-7 py-2 dark:bg-gray-700  border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400 focus:border-transparent"
-        />
-        <span className="absolute  right-1  z-10  hover:text-sky-700 material-symbols-outlined">
-          search
-        </span>
-      </div>
-
-      <div className="flex flex-col sm:flex-row md:justify-between md:items-start sm:justify-center sm:items-start w-full">
+      <div className="flex flex-col mb-4 sm:flex-row md:justify-between md:items-start sm:justify-center mx-2 sm:mx-auto sm:items-start w-full">
         <div className="flex flex-col">
           <Title />
         </div>
 
-        <div className="relative flex items-center mr-2 sm:ml-1 ml-auto mb-3">
-          <input
-            type="text"
-            placeholder="Pesquisar"
-            value={searchValue}
-            onChange={(e) => setSearchValue(e.target.value)}
-            className="pl-2  pr-7 py-2 dark:bg-gray-700  border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400 focus:border-transparent"
-          />
-          <span className="absolute  right-1  z-10  hover:text-sky-700 material-symbols-outlined">
-            search
-          </span>
+        <div className="relative flex items-center pr-2 sm:pr-0 sm:mr-2 justify-end ml-auto w-full sm:w-96 md:w-auto ">
+          <div className="relative flex-1 mr-2 sm:mr-auto ">
+            <input
+              type="text"
+              placeholder="Pesquisar"
+              value={searchValue}
+              onChange={(e) => setSearchValue(e.target.value)}
+              className="w-full pl-2 pr-7 py-2 dark:bg-gray-700  border border-gray-300 rounded-md focus:outline-none focus:ring-2 focus:ring-blue-400 focus:border-transparent"
+            />
+            <span className="absolute  right-1 top-1/2 -translate-y-1/2  z-10  hover:text-sky-700 material-symbols-outlined">
+              search
+            </span>
+          </div>
         </div>
       </div>
 
-      {!searchValue && (
-        <>
-          <HeroPost
-            title={heroPost.title}
-            coverImage={heroPost.coverImage}
-            publishedDate={heroPost.publishedDate}
-            slug={heroPost.slug}
-            metaDesc={heroPost.metaDesc}
-            tags={heroPost.tags}
-          />
-          <h2 className="mb-8 text-2xl md:text-5xl font-bold tracking-tighter leading-tight">
-            Explore Mais
-          </h2>
-        </>
-      )}
-      {morePosts.length > 0 &&
-        morePosts.map((post) => (
-          <MoreStories
-            key={post.slug}
-            title={post.title}
-            coverImage={post.coverImage}
-            publishedDate={post.publishedDate}
-            slug={post.slug}
-            metaDesc={post.metaDesc}
-            tags={post.tags}
-          />
-        ))}
+      <div className="mx-2 sm:mx-0">
+        {!searchValue && (
+          <>
+            <HeroPost
+              title={heroPost.title}
+              coverImage={heroPost.coverImage}
+              publishedDate={heroPost.publishedDate}
+              slug={heroPost.slug}
+              metaDesc={heroPost.metaDesc}
+              tags={heroPost.tags}
+            />
+            <h2 className="mb-8 text-2xl md:text-5xl font-bold tracking-tighter leading-tight">
+              Explore Mais
+            </h2>
+          </>
+        )}
+        {morePosts.length > 0 &&
+          morePosts.map((post) => (
+            <MoreStories
+              key={post.slug}
+              title={post.title}
+              coverImage={post.coverImage}
+              publishedDate={post.publishedDate}
+              slug={post.slug}
+              metaDesc={post.metaDesc}
+              tags={post.tags}
+            />
+          ))}
+      </div>
     </>
   );
 };
